e65789772083156ad3402a59bf80d6709b441858
heyanlu <he.yanl@northeastern.edu>
2024-06-28 19:21:00 -0700

    Initial Commit

---
+++ SyncZone/SyncZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncZoneApp.swift
+//  SyncZone
+//
+//  Created by YL He on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SyncZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
